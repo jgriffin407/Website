f30f73a3cbb1e1a9dd080de9b53d9ff668d47d2c
John Griffin <jgriffin407@gmail.com>
2020-05-06 15:12:55 -0400

    initial commit

---
+++ Flashcard/advsubtraction.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<head>
+	<!--Bootstrap reference -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<meta charset="utf-8">
+ <!-- Responsive code for mobile viewing -->
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ <link rel="stylesheet" href = "style/style.css">	
+
+<script src="jquery-3.5.0.js"></script>
+	<title>Advanced Subtraction</title>
+	<!--Variables-->
+	<script>
+
+		var num1 = Math.floor((Math.random() * 100) + 1);
+		var num2 = Math.floor((Math.random() * 100) + 1);
+		var correctAnswer = num1 - num2;
+		var output;
+	</script>
+<script> 
+
+
+</script>
+
+</head>
+<!--Navbar-->
+<body>
+<div class="container-fluid">
+<ul class="nav nav-pills nav-fill">
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="addition.html" role="button" aria-haspopup="true" aria-expanded="false">Addition</a>
+    <div class ="dropdown-menu">
+    	<a class = "dropdown-item" href="addition.php">Basic</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="intaddition.php">Intermediate</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="advaddition.php">Advanced</a>
+    </div>
+  </li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="subtraction.html" role="button" aria-haspopup="true" aria-expanded="false">Subtraction</a>
+    <div class ="dropdown-menu">
+    	<a class = "dropdown-item" href="subtraction.php">Basic</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="intsubtraction.php">Intermediate</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item disabled" href="advsubtraction.php">Advanced</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="multiplication.html" role="button" aria-haspopup="true" aria-expanded="false">Multiplication</a>
+    <div class ="dropdown-menu">
+    	<a class = "dropdown-item" href="multiplication.php">Basic</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="intmultiplication.php">Intermediate</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="advmultiplication.php">Advanced</a>
+    </div>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="division.html" role="button" aria-haspopup="true" aria-expanded="false">Division</a>
+    <div class ="dropdown-menu">
+    	<a class = "dropdown-item" href="division.php">Basic</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="intdivision.php">Intermediate</a>
+    	<div class = "dropdown-divider"></div>
+    	<a class = "dropdown-item" href="advdivision.php">Advanced</a>
+    </div>
+  </li>
+</ul>
+</div>
+<br/>
+<br/>
+	<!--Main Interaction Title-->
+	<h1 style="text-align: center; color: #c24b4b;">Advanced Subtraction</h1> <br/>
+	<!--Equation that appears on screen-->
+	<h1 style="text-align: center;" id="numbers">
+		<!--Displays equation on screen-->
+		<script>
+			document.write(num1 + " - " + num2);
+		</script>
+		<br/>
+	</h1>
+	
+<br/>
+<br/>
+
+<!--Form bar and buttons-->
+
+<div class="container-sm">
+	<input autofocus="" class="form-control" style="text-align: center;" type="text" id="answer" autocomplete="off";>
+	<br/>
+	<br/>
+</div>
+<div class="container-fluid">
+	<div class="row">
+	<div class="col text-center"><button id="submit" type ="button" class="btn btn-primary btn-lg" onclick="userAnswer();">Submit Answer</button></div></div><br/>
+<div class="container-fluid">
+<div class="row">
+	<div class="col text-center"><button id="reset" type ="button" class="btn btn-primary btn-lg" onclick="nextCard();">Next Card</button></div></div>	
+</div>	
+	<br/>
+<div class="container-fluid">
+<div class="row">
+	<div class="col text-center"><button id="solve" type ="button" class="btn btn-primary btn-lg" onclick="giveUp();">Give Up</button></div></div>	
+</div>	
+<br>
+<br>
+</body>
+
+	<span><h3 style="text-align: center;"  
+		id="output">		
+	</h3></span>
+	
+<script>
+		// Enables enter key functionality //
+		$("#answer").keyup(function(event) {
+			if (event.keyCode === 13) {
+		$("#submit").click();
+		}
+		});
+
+		// Calculates user entry against equation shown //
+		function userAnswer() {
+			var userAnswer = document.getElementById('answer').value;
+			
+			// Validates user entry //
+	if (isNaN(userAnswer)) {output = userAnswer + " " + "is not a number, try again.";} else {
+				// Validates correct answer //
+				if (userAnswer == correctAnswer) {
+					output = "Correct!";} 
+				else {
+					output = "Incorrect";}
+			}
+			
+			// Removes focus from text field after answer is submitted //
+			document.getElementById('answer').blur();
+
+			// Displays result to screen //
+			document.getElementById("output").innerHTML = output;
+
+			}
+		
+
+			// Shows new card //
+		function nextCard() {
+			document.getElementById("output").innerHTML = "";
+			document.getElementById("answer").focus();
+			document.getElementById("answer").value = "";
+	
+			num1 = Math.floor((Math.random() * 100) + 1);
+			num2 = Math.floor((Math.random() * 100) + 1);
+			correctAnswer = num1 - num2;
+			document.getElementById("numbers").innerHTML = (num1 + " - " + num2);
+		}
+			// Enables user to press 'n' key to show next card // 
+		$(document).keyup(function(event) {
+				if (event.keyCode === 78) {
+			$("#reset").click();
+	}
+		});
+
+			// Displays solution to equation // 
+
+		function giveUp() {
+			document.getElementById("output").innerHTML = ("Solution: " + num1 +
+			" - " + num2 + " = " + correctAnswer);		
+		}
+
+
+	</script>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<br><br/><br/><br/>
+<br><br/><br/>
+<footer>&copy; Copyright 2020 John Griffin - All Rights Reserved</footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,373 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>John Griffin | Full Stack Web Developer</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="img/griffin-32x32.png" rel="icon">
+  <link href="img/griffin.png" rel="griffin-icon">
+
+  <!-- Bootstrap CSS File -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+    Theme Name: DevFolio
+    Theme URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
+    Author: BootstrapMade.com
+    License: https://bootstrapmade.com/license/
+  ======================================================= -->
+</head>
+
+<body id="page-top">
+
+  <!--/ Nav Star /-->
+  <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll" href="#page-top"></a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
+        aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <div class="navbar-collapse collapse justify-content-end" id="navbarDefault">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link js-scroll active" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#service">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#work">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--/ Nav End /-->
+
+  <!--/ Intro Skew Star /-->
+  <div id="home" class="intro route bg-image" style="background-color: #2c2828";)>
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="intro-title mb-4">John Griffin</h1>
+          <p class="intro-subtitle"><span class="text-slider-items">Full-Stack Web Developer,PC Repair Technician</span><strong class="text-slider"></strong></p>
+          <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+  <section id="about" class="about-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="box-shadow-full">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="row">
+                  <div class="col-sm-6 col-md-5">
+                    <div class="about-img">
+                      <img src="img/testimonial-2.jpg" class="img-fluid rounded b-shadow-a" alt="">
+                    </div>
+                  </div>
+                  <div class="col-sm-6 col-md-7">
+                    <div class="about-info">
+                      <p><span class="title-s">Name: </span> <span>John Griffin</span></p>
+                      <p><span class="title-s">Profile: </span> <span>Full Stack Developer</span></p>
+                      <p><span class="title-s">Email: </span> <span>jgriffin407@gmail.com</span></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="skill-mf">
+                  <p class="title-s">Experience</p>
+                  <span>HTML</span> <span class="pull-right">85%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                  <span>CSS3</span> <span class="pull-right">25%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                  <span>PHP</span> <span class="pull-right">40%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                  <span>JAVASCRIPT</span> <span class="pull-right">50%</span>
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0"
+                      aria-valuemax="100"></div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="about-me pt-4 pt-md-0">
+                  <div class="title-box-2">
+                    <h5 class="title-left">
+                      About me
+                    </h5>
+                  </div>
+                  <p class="lead">
+                    I'm a self-taught full-stack developer. This page is under construction as I continually add more content.
+                  </p>
+                  <p class="lead">
+                    <!--Add some content here-->
+                  </p>
+                  <p class="lead">
+                    <!--Add some more content here-->
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--/ Section Services Star /-->
+  <section id="service" class="services-mf route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Services
+            </h3>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-monitor"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">PC Service</h2>
+              <p class="s-description text-center">
+                <ul>
+                  <li>New PC Setup</li>
+                  <ul>
+                    <li>Remove bloatware</li>
+                    <li>Create accounts</li>
+                  </ul>
+                  <li>Hardware and software installations</li>
+                  <li>Diagnostics and repair</li>
+                  <li>Virus removals</li>
+                  <li>Data backups</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-code-working"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Web Development</h2>
+              <p class="s-description text-center">
+                <ul>
+                  <li>Front End Development</li>
+                  <ul>
+                    <li>HTML5</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                  </ul>
+                  <li>Back End Development</li>
+                    <ul>
+                      <li>SQL Server</li>
+                      <li>Python</li>
+                      <li>PHP</li>
+                    </ul>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-android-phone-portrait"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Responsive Design</h2>
+              <p class="s-description text-center">
+                Creating responsive webpages that are viewable on:
+                <ul>
+                  <li>Desktop Environments</li>
+                  <li>iOS</li>
+                  <li>Android</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Section Services End /-->
+  <!--/ Section Portfolio Star /-->
+  <section id="work" class="portfolio-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Portfolio
+            </h3>
+            <p class="subtitle-a">
+              <em>Click an image below if you'd like to see some of my work.</em>
+            </p>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="work-box">
+              <div class="work-img">
+                <a href="Flashcard/addition.php">
+                <img src="img/flashcard.png" alt="mathflashcard">
+              </a>
+              </div>
+              <div class="work-content">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h2 class="w-title">Math Flashcard</h2>
+                    <div class="w-more">
+                      <span class= "w-date">May 4, 2020</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Section Portfolio End /-->
+  <!--/ Section Contact-Footer Star /-->
+  <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/overlay-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="contact-mf">
+            <div id="contact" class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="title-box-2">
+                  </div>
+                  <div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="title-box-2 pt-4 pt-md-0">
+                    <h5 class="title-left">
+                      Get in Touch
+                    </h5>
+                  </div>
+                  <div class="more-info">
+                    <p class="lead">
+                      You can reach me via email or any of my social media sites below.
+                    </p>
+                    <ul class="list-ico">
+                      <li><span class="ion-email"></span>
+                        <a href="mailto:jgriffin407@gmail.com">jgriffin407@gmail.com</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="socials">
+                    <ul>
+                      <li><a href="https://www.facebook.com/jgriff1289"><span class="ico-circle" target="_blank"><i class="ion-social-facebook" title="Facebook"></i></span></a></li>
+                      <li><a href="https://www.linkedin.com/in/john-griffin-358b7172/" target="_blank"><span class="ico-circle"><i class="ion-social-linkedin" title="LinkedIn"></i></span></a>
+                      <li><a href="https://github.com/jgriffin407" target="_blank"><span class="ico-circle"><i class="ion-social-github" title="GitHub"></i></span></a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="copyright-box">
+              <p class="copyright">&copy; Copyright 2020 <strong>John Griffin</strong>. All Rights Reserved</p>
+              <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
+                -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </section>
+  <!--/ Section Contact-footer End /-->
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <div id="preloader"></div>
+
+  <!-- JavaScript Libraries -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/popper/popper.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/counterup/jquery.waypoints.min.js"></script>
+  <script src="lib/counterup/jquery.counterup.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/typed/typed.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
+</body>
+</html>
