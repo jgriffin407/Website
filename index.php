bee4e20ffddc6e53d6efd3f34409a2053d717354
John Griffin <jgriffin407@gmail.com>
2020-05-08 15:25:29 -0400

    set portfolio links to open in new tab

--- index.php
+++ index.php
@@ -258,7 +258,7 @@
       <div class="row">
         <div class="col-md-4">
           <div class="work-box">
-            <a href="Flashcard/addition.php">
+            <a href="Flashcard/addition.php" target="_blank">
               <div class="work-img">
                 <img src="img/flashcard.png" alt="mathcard" class="img-fluid">
               </div>
@@ -284,7 +284,7 @@
         <!--Portfolio box#2 starts here-->
         <div class="col-md-4">
           <div class="work-box">
-            <a href="Quiz/index.php">
+            <a href="Quiz/index.php" target="_blank">
               <div class="work-img">
                 <img src="img/trivia.jpg" alt="trivia" class="img-fluid">
               </div>
