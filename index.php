82e41286d1d0bf9b528f879c5f0afc9d2fc3ce3a
John Griffin <jgriffin407@gmail.com>
2020-05-24 13:31:39 -0400

    added contact form

--- index.php
+++ index.php
@@ -413,18 +413,54 @@
   <section class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(img/overlay-bg.jpg)">
     <div class="overlay-mf"></div>
     <div class="container">
-      <div class="row" >
+      <div class="row">
         <div class="col-sm-12">
           <div class="contact-mf">
             <div id="contact" class="box-shadow-full">
               <div class="row">
                 <div class="col-md-6">
                   <div class="title-box-2">
+                    <h5 class="title-left">
+                      Send Message
+                    </h5>
                   </div>
                   <div>
+                      <form action="mailto:john@johngriffin.dev" method="post" enctype="text/plain" role="form" class="contactForm">
+                      <div id="sendmessage">Your message has been sent. Thank you!</div>
+                      <div id="errormessage"></div>
+                      <div class="row">
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:2" data-msg="Name must contain at least 2 characters" />
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                            <div class="form-group">
+                              <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:2" data-msg="Subject must contain at least 2 characters" />
+                              <div class="validation"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-12 mb-3">
+                          <div class="form-group">
+                            <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something in the message box" placeholder="Message"></textarea>
+                            <div class="validation"></div>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <button type="submit" class="button button-a button-big button-rounded">Send Message</button>
+                        </div>
+                      </div>
+                    </form>
                   </div>
                 </div>
-                <div class="col-md-12">
+                <div class="col-md-6">
                   <div class="title-box-2 pt-4 pt-md-0">
                     <h5 class="title-left">
                       Get in Touch
@@ -432,21 +468,16 @@
                   </div>
                   <div class="more-info">
                     <p class="lead">
-                      You can reach me via email or any of my social media sites below.
-                    </p>
-                    <ul class="list-ico">
-                      <li><span class="ion-email"></span>
-                        <a href="mailto:john@johngriffin.dev">Click to send an Email</a>
-                      </li>
-                    </ul>
+                        You can reach me via the form on the left or any of my social media pages below.
+                  </p>
                   </div>
                   <div class="socials">
                     <ul>
-                       <li><a href="https://www.facebook.com/jgriff1289" target="_blank"><span class="ico-circle"><i class="ion-social-facebook" title="Facebook"></i></span></a>
-                      <li><a href="https://www.linkedin.com/in/john-griffin-358b7172/" target="_blank"><span class="ico-circle"><i class="ion-social-linkedin" title="LinkedIn"></i></span></a>
-                      <li><a href="https://github.com/jgriffin407" target="_blank"><span class="ico-circle"><i class="ion-social-github" title="GitHub"></i></span></a>
-                      </li>
-                    </ul>
+                        <li><a href="https://www.facebook.com/jgriff1289" target="_blank"><span class="ico-circle"><i class="ion-social-facebook" title="Facebook"></i></span></a>
+                       <li><a href="https://www.linkedin.com/in/john-griffin-358b7172/" target="_blank"><span class="ico-circle"><i class="ion-social-linkedin" title="LinkedIn"></i></span></a>
+                       <li><a href="https://github.com/jgriffin407" target="_blank"><span class="ico-circle"><i class="ion-social-github" title="GitHub"></i></span></a>
+                       </li>
+                     </ul>
                   </div>
                 </div>
               </div>
@@ -455,25 +486,6 @@
         </div>
       </div>
     </div>
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="copyright-box">
-              <p class="copyright">&copy; Copyright 2020 <strong>John Griffin</strong>. All Rights Reserved</p>
-              <div class="credits">
-                <!--
-                  All the links in the footer should remain intact.
-                  You can delete the links only if you purchased the pro version.
-                  Licensing information: https://bootstrapmade.com/license/
-                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
-                -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </footer>
   </section>
   <!--/ Section Contact-footer End /-->
